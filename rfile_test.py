3f908358fb180bec03fd98f57b5f8d51afe6c252
Lili Feng <lili.feng@colorado.edu>
2016-04-29 18:04:09 -0600

    modify vmodel

--- rfile_test.py
+++ rfile_test.py
@@ -1,6 +1,6 @@
 import vmodel
 
 rmodel=vmodel.rModel('/projects/life9360/code/sw4-v1.1/examples/rfile/berkeley.rfile');
-rmodel.write('/projects/life9360/code/sw4-v1.1/examples/rfile/berkeley_lf.rfile')
+# rmodel.write('/projects/life9360/code/sw4-v1.1/examples/rfile/berkeley_lf.rfile')
 # rmodel=vmodel.rModel('/projects/life9360/code/sw4-v1.1/examples/rfile/berkeley_lf.rfile');
 # rmodel
